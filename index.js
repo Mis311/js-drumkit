4befbee1f657c2d54ba15f08c4a8bfa49b969eda
Mis311 <55878506+Mis311@users.noreply.github.com>
2022-11-03 16:35:12 +0900

    complete drumkit

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-function handleClick() {
-  alert("I got clicked!"); // do something
-}
-
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var c = new Audio("sounds/crash.mp3");
+      c.play();
+      break;
+    case "k":
+      var k = new Audio("sounds/kick-bass.mp3");
+      k.play();
+      break;
+    case "l":
+      var l = new Audio("sounds/snare.mp3");
+      l.play();
+    default:
+      console.log(key);
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
 // var audio = new Audio("sounds/tom-1.mp3");
-// audio.play();
+// audio.play(); wasdjkl
